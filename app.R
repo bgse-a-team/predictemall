048ae1bc4ba035ed260fd414daf8c93c184ee640
Akhil Lohia <akhil.lohia@gmail.com>
2016-12-01 03:04:07 +0100

    code cleaning, type implementation

--- app.R
+++ app.R
@@ -6,6 +6,9 @@ library(shinythemes)
 library(mapproj)
 library(ggplot2)
 library(leaflet)
+library(googleVis)
+library(plotrix)
+library(sqldf)
 
 options(shiny.error = function(){})
 
@@ -16,141 +19,147 @@ css <- "
   content: 'Please Select an Option'; }
 }
 "
-con <- dbConnect(MySQL(),user = "trainer", password = "master", host = "127.0.0.1", dbname = "project")
-#con <- dbConnect(MySQL(),user = "almysql", password = "pass", host = "127.0.0.1", dbname = "project")
+#con <- dbConnect(MySQL(),user = "trainer", password = "master", host = "127.0.0.1", dbname = "project")
+con <- dbConnect(MySQL(),user = "almysql", password = "pass", host = "127.0.0.1", dbname = "project")
 #con <- dbConnect(MySQL(),user = "root", password = "password", host = "127.0.0.1", dbname = "project")
 
-# Define UI for application that draws a histogram
-ui <- shinyUI(navbarPage(tags$style(type="text/css", css),"Pokemon Go Predictor", theme = shinytheme("slate"),
-                         tabPanel("Descriptive",
-                                  sidebarLayout(
-                                    sidebarPanel("Select continent and time period to see pokemon spawns",
-                                      selectInput("select_continent","Select Continent",selected=NA,dbGetQuery(con,"SELECT DISTINCT continent FROM poke_spawns WHERE country IS NOT NULL ORDER BY continent")),
-                                      uiOutput("selectedContinent"),
-                                      uiOutput("Types"),
-                                      selectInput("select_class","Select Pokemon",dbGetQuery(con,"SELECT DISTINCT a.Name FROM (poke_spawns a inner join pkmn_info b on b.id = a.pokemonId)  WHERE a.Name IS NOT NULL ORDER BY a.Name")),
-                                      uiOutput("pics",align="center"),
-                                      sliderInput("time", "Period of Time", min = as.Date("2016-08-04"), max = as.Date("2016-10-12"), 
-                                                  value = c(as.Date("2016-08-01"), Sys.Date())),
-                                      actionButton("action", label = "(Pokemon) GO")
-                                    ),
-                                    mainPanel(
-                                      "Spawns in selected continent",
-                                      leafletOutput("map_continent"),
-                                      "Spawns in selected country",
-                                      leafletOutput("map_country")
-                                    )
-                                  )        
-                         ),
-                         tabPanel("Analysis",
-                                  sidebarLayout(
-                                    sidebarPanel(
-                                      selectInput("select_type","Select Type of Pokemon",dbGetQuery(con,"SELECT DISTINCT `type1` FROM (poke_spawns inner join pkmn_info on id = pokemonId)  WHERE `type1` IS NOT NULL ORDER BY `type1`")),
-                                      selectInput("select_weather","Select Weather",dbGetQuery(con,"SELECT DISTINCT weather FROM poke_spawns WHERE weather IS NOT NULL ORDER BY weather")
-                                                  )),
-                                      mainPanel("The most important variables to find this pokemon type is")
-                                  )),
-                         tabPanel("Details",
-                                  fluidPage(
-                                    # Some custom CSS for a smaller font for preformatted text
-                                    tags$head(
-                                      tags$style(HTML("
-                                                       pre, table.table {
-                                                       font-size: smaller;
-                                                       }
-                                                       "))
-                                    ),
-                                    
-                                    fluidRow(
-                                      column(width = 4, wellPanel(
-                                        radioButtons("plot_type", "Plot type",
-                                                     c("base", "ggplot2")
-                                        )
-                                      )),
-                                      column(width = 4,
-                                             # In a plotOutput, passing values for click, dblclick, hover, or brush
-                                             # will enable those interactions.
-                                             plotOutput("plot1", height = 350,
-                                                        # Equivalent to: click = clickOpts(id = "plot_click")
-                                                        click = "plot_click",
-                                                        dblclick = dblclickOpts(
-                                                          id = "plot_dblclick"
-                                                        ),
-                                                        hover = hoverOpts(
-                                                          id = "plot_hover"
-                                                        ),
-                                                        brush = brushOpts(
-                                                          id = "plot_brush"
-                                                        )
-                                             )
-                                      )
-                                    ),
-                                    fluidRow(
-                                      column(width = 3,
-                                             verbatimTextOutput("click_info")
-                                      ),
-                                      column(width = 3,
-                                             verbatimTextOutput("dblclick_info")
-                                      ),
-                                      column(width = 3,
-                                             verbatimTextOutput("hover_info")
-                                      ),
-                                      column(width = 3,
-                                             verbatimTextOutput("brush_info")
-                                      )
-                                    )
-                                  ))
+# Define UI for application
+ui <- shinyUI(navbarPage(
+  title = "Pokemon Go Predictor",
+  #tags$style(type="text/css", css),
+  theme = shinytheme("slate"),
+  tabPanel("Descriptive",
+           sidebarLayout(
+             sidebarPanel("Select fields to see pokemon spawns",
+                          selectInput("select_continent","Select Continent",dbGetQuery(con,"SELECT DISTINCT continent FROM poke_spawns WHERE country IS NOT NULL ORDER BY continent")),
+                          uiOutput("selectedContinent"),
+                          uiOutput("Types"),
+                          selectInput("select_class","Select Pokemon",dbGetQuery(con,"SELECT DISTINCT a.Name FROM (poke_spawns a inner join pkmn_info b on b.id = a.pokemonId)  WHERE a.Name IS NOT NULL ORDER BY a.Name")),
+                          uiOutput("pics",align="center"),
+                          sliderInput("time", "Period of Time", min = as.Date("2016-08-04"), max = as.Date("2016-10-12"), 
+                                      value = c(as.Date("2016-08-01"), Sys.Date())),
+                          actionButton("action", label = "(Pokemon) GO")
+             ),
+             mainPanel(
+               "Spawns in selected continent",
+               leafletOutput("map_continent"),
+               "Spawns in selected country",
+               leafletOutput("map_country")
+             )
+           )        
+  ),
+  tabPanel("Analysis",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput("select_type","Select Type of Pokemon",dbGetQuery(con,"SELECT DISTINCT `type1` FROM (poke_spawns inner join pkmn_info on id = pokemonId)  WHERE `type1` IS NOT NULL ORDER BY `type1`")),
+               selectInput("select_weather","Select Weather",dbGetQuery(con,"SELECT DISTINCT weather FROM poke_spawns WHERE weather IS NOT NULL ORDER BY weather")
+               )),
+             mainPanel("The most important variables to find this pokemon type is")
+           )),
+  tabPanel("Details",
+           fluidPage(
+             # Some custom CSS for a smaller font for preformatted text
+             # tags$head(
+             #   tags$style(HTML("
+             #                                           pre, table.table {
+             #                                           font-size: smaller;
+             #                                           }
+             #                                           "))
+             # ),
+             
+             fluidRow(
+               column(width = 4, wellPanel(
+                 radioButtons("plot_type", "Plot type",
+                              c("base", "ggplot2")
+                 )
+               )),
+               column(width = 4,
+                      # In a plotOutput, passing values for click, dblclick, hover, or brush
+                      # will enable those interactions.
+                      plotOutput("plot1", height = 350,
+                                 # Equivalent to: click = clickOpts(id = "plot_click")
+                                 click = "plot_click",
+                                 dblclick = dblclickOpts(
+                                   id = "plot_dblclick"
+                                 ),
+                                 hover = hoverOpts(
+                                   id = "plot_hover"
+                                 ),
+                                 brush = brushOpts(
+                                   id = "plot_brush"
+                                 )
+                      )
+               )
+             ),
+             fluidRow(
+               column(width = 3,
+                      verbatimTextOutput("click_info")
+               ),
+               column(width = 3,
+                      verbatimTextOutput("dblclick_info")
+               ),
+               column(width = 3,
+                      verbatimTextOutput("hover_info")
+               ),
+               column(width = 3,
+                      verbatimTextOutput("brush_info")
+               )
+             )
+           ))
 ))
 
 server <- shinyServer(function(input, output) {
+  pokemonGO <- read.csv('pokemonGO.csv')
+  pokemonGO$Name<-as.character(pokemonGO$Name)
+  
   selectedData <- reactive({
-    dbGetQuery(con,sprintf("SELECT * FROM poke_spawns WHERE country = \'%s\'",as.character(input$select_country)))
+    dbGetQuery(con,sprintf("SELECT longitude,latitude FROM poke_spawns WHERE country = \'%s\'",as.character(input$select_country)))
   })
   
   selectedData2 <- reactive({
-    dbGetQuery(con,sprintf("SELECT * FROM poke_spawns WHERE continent = \'%s\'",as.character(input$select_continent)))
+    dbGetQuery(con,sprintf("SELECT longitude,latitude FROM poke_spawns WHERE continent = \'%s\' and `type1` = \'%s\'",as.character(input$select_continent),as.character(input$select_typeofpoke)))
   })
-
+  
   selectedData3 <- reactive({
-    dbGetQuery(con,sprintf("SELECT * FROM poke_spawns inner join pkmn_info on id = pokemonId WHERE continent = \'%s\' and country = \'%s\' and `type1` = \'%s\'",as.character(input$select_continent),as.character(input$select_country),as.character(input$select_typeofpoke)))
+    dbGetQuery(con,sprintf("SELECT longitude,latitude FROM poke_spawns WHERE continent = \'%s\' and country = \'%s\' and `type1` = \'%s\'",as.character(input$select_continent),as.character(input$select_country),as.character(input$select_typeofpoke)))
   })
   
   output$selectedContinent <- renderUI({
-    countries<-dbGetQuery(con, sprintf("SELECT DISTINCT country FROM poke_spawns WHERE continent = \'%s\' ORDER BY country", as.character(input$select_continent))) 
+    countries<-dbGetQuery(con, sprintf("SELECT DISTINCT country FROM poke_spawns WHERE continent = \'%s\' ORDER BY country", as.character(input$select_continent)))
     conditionalPanel("input.select_continent", selectInput("select_country","Select Country", countries))
-    })
+  })
 
   output$Types <- renderUI({
-    types<-dbGetQuery(con,"SELECT DISTINCT `type1` FROM (poke_spawns inner join pkmn_info on id = pokemonId)  WHERE `type1` IS NOT NULL ORDER BY `type1`") 
+    types<-dbGetQuery(con,"SELECT DISTINCT `type1` FROM (poke_spawns inner join pkmn_info on id = pokemonId)  WHERE `type1` IS NOT NULL ORDER BY `type1`")
     conditionalPanel("input.select_country", selectInput("select_typeofpoke","Select Type of Pokemon", types))
   })
   
-  try_continent <- eventReactive(input$action, {
-    as.data.frame(selectedData2()[,5:4])
+  get_continent <- eventReactive(input$action, {
+    as.data.frame(selectedData2())
   })
   
+  get_country <- eventReactive(input$action, {
+    as.data.frame(selectedData3())
+  })
   
   output$map_continent <- renderLeaflet({
-    leaflet(try_continent()) %>%
+    leaflet(get_continent()) %>%
       addTiles() %>%
       addCircles()
   })
   
   output$map_country <- renderLeaflet({
-    input$action
-    isolate(
-    leaflet(as.data.frame(selectedData()[,5:4])) %>%
+    leaflet(get_country()) %>%
       addTiles() %>%
-      addCircles() 
-  )
+      addCircles()
   })
-
+  
   output$pics<-renderUI({
     
     tags$img(src = pokemonGO[pokemonGO$Name==input$select_class,7], alt = "photo",height="200", width="200")
     
   })
-
+  
   selectedinfo <- reactive({
     dbGetQuery(con,sprintf("SELECT * FROM poke_spawns WHERE class = \'%s\'",as.character(input$select_class)))
   })
@@ -195,7 +204,3 @@ server <- shinyServer(function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
-
-
-
